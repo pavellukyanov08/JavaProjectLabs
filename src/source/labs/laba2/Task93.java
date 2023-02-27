c07dc805835e86c325aacbf4ce15bb901e39bf4e
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-27 11:55:33 +0700

    Task93

---
+++ src/source/labs/laba2/Task93.java
@@ -0,0 +1,18 @@
+package source.labs.laba2;
+
+//import java.util.Scanner;
+public class  Task93 {
+    public static void main(String[] args) {
+//        int i, n;
+//        float q, r, b, c, d;
+//        double[] x = new double[15];
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите действительные числа: "); float q, r, b, c, d = in.nextFloat();
+////        x[0] = c;
+////        x[1] = d;
+//        System.out.print("Введите натуральное число: "); int n = in.nextInt();
+
+
+
+    }
+}
