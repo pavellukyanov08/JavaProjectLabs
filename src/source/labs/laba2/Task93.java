ddb44595d75d0470469f10918e6ce42bec1a4f47
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-27 12:45:41 +0700

    Task 93 is completed

--- src/source/labs/laba2/Task93.java
+++ src/source/labs/laba2/Task93.java
@@ -1,18 +1,27 @@
 package source.labs.laba2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 //import java.util.Scanner;
 public class  Task93 {
     public static void main(String[] args) {
-//        int i, n;
-//        float q, r, b, c, d;
-//        double[] x = new double[15];
-//        Scanner in = new Scanner(System.in);
-//        System.out.print("Введите действительные числа: "); float q, r, b, c, d = in.nextFloat();
-////        x[0] = c;
-////        x[1] = d;
-//        System.out.print("Введите натуральное число: "); int n = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите натуральное число: ");
+        int n = in.nextInt();
+        long[] x = new long[15];
+        long q, r, b, d, c;
+        q = in.nextInt();
+        r = in.nextInt();
+        b = in.nextInt();
+        d = in.nextInt();
+        c = in.nextInt();
+        x[0] = c;
+        x[1] = d;
 
-
-
+        for (int i = 2; i <= n; i++) {
+            x[i] = (q * x[i - 1] + r * x[i - 2] + b);
+        }
+        System.out.println("Ответ: " + Arrays.toString(x));
     }
 }
